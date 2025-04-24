22216bad6eb2b1afb0f3c39a4fe316de6ded71f0
abc <abc@gmail.com>
2025-04-24 14:15:56 +0800

    0424

--- src/static/dist/js/generic_classifier/dashboard/main.js
+++ src/static/dist/js/generic_classifier/dashboard/main.js
@@ -3,192 +3,166 @@ ai.DashboardGC = {
         ai.DashboardGC.setPath();
     },
     setPath: function () {
-        console.log(" dashboard");
-            const categories = ['ECG', 'XRAY', 'MRI', 'CT'];
-            const ecgRecords = 800035;
-            const xrayRecords = 4999;
-            const mriRecords = 1425;
+        console.log(" dashboard"); 
+            fetch('http://127.0.0.1:8889/json/dashboard.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json(); // 解析 JSON 資料
+            })
+            .then(data => {
+                const records = data.data.stat.records.categories;
+                const recordsdata = data.data.stat.records.data;
         
-            Highcharts.chart('Total-Records-Across-Modalities', {
-                chart: {
-                    type: 'bar',
-                    backgroundColor:'#F7F7F7'
-                  },
-                  title: {
-                    text: 'Total Records Across Modalities'
-                  },
-                  xAxis: {
-                    categories: categories,
-                    labels: {
-                      style: { color: '#008687' }
-                    },
-                    lineColor: '#008687'
-                  },
-                  yAxis: {
-                    min: 0,
-                    title: {
-                      text: 'Total Records',
-                      align: 'high',
-                      style: { color: '#008687' }
-                    },
-                    labels: {
-                      overflow: 'justify'
-                    }
-                  },
-                  tooltip: {
-                    valueSuffix: 'Records'
-                  },
-                  plotOptions: {
-                    bar: {
-                      dataLabels: {
-                        enabled: true
-                      }
-                    }
-                  },
-                  series: [{
-                    name: '總資料數',
-                    data: [ecgRecords, xrayRecords, mriRecords],
-                    color: '#008687'
-                  }]
+                const maxValue = Math.max(...recordsdata);
+                const recordsData = recordsdata.map(val => {
+                    const ratio = val / maxValue;
+                    const color = `hsl(180, 50%, ${55 - ratio * 30}%)`; 
+                    return {
+                        y: val,
+                        color: color
+                    };
                 });
-                
-            const totalSubjects = [161352, 2149, 108, 32];
-            const avgSubjects = [4.958320938073281, 2.326198231735691, 13.194444444444445, 2.1875];
-            Highcharts.chart('Total-Subjects-Across-Modalities', {
-                chart: {
-                    type: 'bar',
-                    backgroundColor: '#F7F7F7'
-                },
-                title: {
-                    text: 'Total Subjects Across Modalities'
-                },
-                xAxis: {
-                    categories: categories,
-                    title: null,
-                    labels: { style: { color: '#008687' } },
-                    lineColor: '#008687'
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                    text: '總項目數',
-                    align: 'high',
-                    style: { color: '#008687' }
+
+                Highcharts.chart('Total-Records-Across-Modalities', {
+                    chart: {
+                        type: 'bar',
+                        backgroundColor: '#F7F7F7'
                     },
-                    labels: { overflow: 'justify' }
-                },
-                tooltip: {
-                    valueSuffix: ' 人'
-                },
-                plotOptions: {
-                    bar: {
-                    dataLabels: {
-                        enabled: true
-                    }
-                    }
-                },
-                series: [{
-                    name: 'Subjects',
-                    data: totalSubjects,
-                    color: '#008687'
-                }]
+                    title: {
+                        text: 'Total Records Across Modalities',
+                        style: {color: '#008687'}
+                    },
+                    xAxis: {
+                        categories: records,
+                        labels: {
+                            style: {color: '#008687'}
+                        },
+                        lineColor: '#008687'
+                    },
+                    yAxis: {
+                        min: 0, 
+                        tickInterval: 100000,
+                        title: {text: '總資料數', style: {color: '#008687'}}
+                    },
+                    legend: {enabled: false},
+                    series: [{
+                        name: '總資料數',
+                        data: recordsData ,
+                        dataLabels: {
+                            enabled: true,
+                        }
+                    }]
+                });
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+            });
+            
+            fetch('http://127.0.0.1:8889/json/dashboard.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json(); // 解析 JSON 資料
+            })
+            .then(data => {
+                const subjects = data.data.stat.subjects.categories;
+                const subjectsdata = data.data.stat.subjects.data;
+                const maxValue = Math.max(...subjectsdata);
+                const subjectsData = subjectsdata.map(val => {
+                    const ratio = val / maxValue;
+                    const color = `hsl(180, 50%, ${55 - ratio * 30}%)`; 
+                    return {
+                        y: val,
+                        color: color
+                    };
+                });
+                Highcharts.chart('Total-Subjects-Across-Modalities', {
+                    chart: {
+                        type: 'bar',
+                        backgroundColor: '#F7F7F7'
+                    },
+                    title: {
+                        text: 'Total Subjects Across Modalities',
+                        style: {color: '#008687'}
+                    },
+                    xAxis: {
+                        categories: subjects,
+                        labels: {
+                            style: {color: '#008687'}
+                        },
+                        lineColor: '#008687'
+                    },
+                    yAxis: {
+                        min: 0, 
+                        tickInterval: 25000,
+                        title: {text: '總項目數', style: {color: '#008687'}}
+                    },
+                    legend: {enabled: false},
+                    series: [{
+                        name: '總項目數',
+                        data: subjectsData ,
+                        dataLabels: {
+                            enabled: true,
+                        }
+                    }]
+                });      
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+            });
+
+            fetch('http://127.0.0.1:8889/json/dashboard.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json(); // 解析 JSON 資料
+            })
+            .then(data => {
+                const avg_subjects = data.data.stat.avg_subjects.categories;
+                const avg_subjectsdata = data.data.stat.avg_subjects.data;
+            
+                const maxValue = Math.max(...avg_subjectsdata);
+                const avgData = avg_subjectsdata.map(val => {
+                    const ratio = val / maxValue;
+                    const color = `hsl(180, 50%, ${55 - ratio * 30}%)`; 
+                    return {
+                        y: val,
+                        color: color
+                    };
                 });
             
-                // 平均項目數
                 Highcharts.chart('Average-Subjects-Across-Modalities', {
                     chart: {
                         type: 'bar',
                         backgroundColor: '#F7F7F7'
                     },
                     title: {
-                        text: 'Average Subjects Across Modalities'
+                        text: 'Average Subjects Across Modalities',
+                        style: { color: '#008687' }
                     },
                     xAxis: {
-                        categories: categories,
-                        title: null,
+                        categories: avg_subjects,
                         labels: { style: { color: '#008687' } },
                         lineColor: '#008687'
                     },
                     yAxis: {
                         min: 0,
-                        title: {
-                        text: '平均項目數',
-                        align: 'high',
-                        style: { color: '#008687' }
-                        },
-                        labels: { overflow: 'justify' }
+                        tickInterval: 2,
+                        title: { text: '平均項目數', style: { color: '#008687' } }
                     },
-                    tooltip: {
-                        valueSuffix: ' 項目'
-                    },
-                    plotOptions: {
-                        bar: {
-                        dataLabels: {
-                            enabled: true
-                        }
-                        }
-                    },
+                    legend: { enabled: false },
                     series: [{
-                        name: 'Average',
-                        data: avgSubjects,
-                        colorByPoint: true,
-                        colors: ['#004D4D','#00C9A7', '#00C9D3','#00C2A0']
+                        name: '平均項目數',
+                        data: avgData,
+                        dataLabels: { enabled: true }
                     }]
-                    });
-            // fetch('http://127.0.0.1:8889/json/dashboard.json')
-            // .then(response => {
-            //     if (!response.ok) {
-            //         throw new Error('Network response was not ok');
-            //     }
-            //     return response.json(); // 解析 JSON 資料
-            // })
-            // .then(data => {
-            //     const ecgRecords = data.data.ecg_stats.total_ecg_records;
-            //     const xrayRecords = data.data.xray_stats.total_xray_files;
-            //     const mriRecords = data.data.mri_stats.total_mri_files;
+                });
+            })
             
-            //     Highcharts.chart('Total-Records-Across-Modalities', {
-            //         chart: {
-            //         type: 'column',
-            //         backgroundColor:'#F7F7F7'
-            //         },
-            //         title: {
-            //         text: 'Total Records Across Modalities',
-            //         style: { color: '#008687' }
-            //         },
-            //         xAxis: {
-            //         categories: ['ECG', 'X-ray', 'MRI'],
-            //         title: {
-            //             text: 'Modality'
-            //         },
-            //         labels: {
-            //             style: { color: '#008687' }
-            //         }
-            //         },
-            //         yAxis: {
-            //         title: {
-            //             text: 'Record Count',
-            //             style: { color: '#008687' }
-            //         },
-            //         labels: {
-            //             formatter: function () {
-            //             return this.value.toLocaleString(); // 加上千分位
-            //             }
-            //         }
-            //         },
-            //         tooltip: {
-            //         pointFormat: '<b>{point.y:,.0f} records</b>'
-            //         },
-            //         series: [{
-            //         name: 'Total Records',
-            //         data: [ecgRecords, xrayRecords, mriRecords],
-            //         colorByPoint: true,
-            //         colors: ['#008687', '#00b89b', '#00c5a9']
-            //         }]
-            //     });
-            // })
-        
-
             fetch('http://127.0.0.1:8889/json/dashboard.json')
             .then(response => {
                 if (!response.ok) {
@@ -199,7 +173,16 @@ ai.DashboardGC = {
             .then(data => {
                 let bins = data.data.age_distribution.bins;
                 let agecounts = data.data.age_distribution.counts;
-
+                
+                const maxValue = Math.max(...agecounts);
+                const ageData = agecounts.map(val => {
+                    const ratio = val / maxValue;
+                    const color = `hsl(180, 50%, ${55 - ratio * 30}%)`; 
+                    return {
+                        y: val,
+                        color: color
+                    };
+                });
                 Highcharts.chart('age-distribution', {
                     chart: {
                         type: 'column',
@@ -242,8 +225,7 @@ ai.DashboardGC = {
                     },
                     series: [{
                         name: '人數',
-                        data: agecounts, // 使用 JSON 提供的數據
-                        color: '#008687'
+                        data: ageData
                     }]
                 });
             })
@@ -261,7 +243,6 @@ ai.DashboardGC = {
             .then(data => {
                 let female = data.data.gender_distribution.female;
                 let male = data.data.gender_distribution.male;
-
                 Highcharts.chart('gender-distribution', {
                     chart: {
                         type: 'pie',
@@ -300,6 +281,16 @@ ai.DashboardGC = {
                 let icddata = data.data.icd_distribution;
                 let categories = Object.keys(icddata);
                 let seriesData = Object.values(icddata); 
+
+                const maxValue = Math.max(...seriesData);
+                const icdData = seriesData.map(val => {
+                    const ratio = val / maxValue;
+                    const color = `hsl(180, 50%, ${55 - ratio * 30}%)`; 
+                    return {
+                        y: val,
+                        color: color
+                    };
+                });
                 Highcharts.chart('icd-distribution', {
                     chart: {
                         type: 'bar',
@@ -322,8 +313,7 @@ ai.DashboardGC = {
                     legend: {enabled: false},
                     series: [{
                         name: '病例數',
-                        data: seriesData,
-                        color: '#008687',
+                        data: icdData,
                         dataLabels: {
                             enabled: true,
                         }
@@ -345,6 +335,16 @@ ai.DashboardGC = {
             .then(data => {
                 let lab_test_names = data.data.lab_tests_distribution.lab_test_names;
                 let labcounts = data.data.lab_tests_distribution.counts; 
+                const maxValue = Math.max(...labcounts);
+                const minValue = Math.min(...labcounts);
+                const labData = labcounts.map(val => {
+                    const ratio =(val - minValue) / (maxValue - minValue);
+                    const color = `hsl(180,55%, ${55 - ratio * 30}%)`; 
+                    return {
+                        y: val,
+                        color: color
+                    };
+                });
                 Highcharts.chart('lab-tests-distribution', {
                     chart: {
                         type: 'bar',
@@ -366,8 +366,7 @@ ai.DashboardGC = {
                     legend: {enabled: false},
                     series: [{
                         name: '病例數',
-                        data: labcounts,
-                        color: '#008687',
+                        data: labData,
                         dataLabels: {
                             enabled: true,
                         }
@@ -390,6 +389,15 @@ ai.DashboardGC = {
                 let medications = data.data.medication_distribution.medications;
                 let medicationcounts = data.data.medication_distribution.counts;
 
+                const maxValue = Math.max(...medicationcounts);
+                const medData = medicationcounts.map(val => {
+                    const ratio = val / maxValue;
+                    const color = `hsl(180, 50%, ${55 - ratio * 30}%)`; 
+                    return {
+                        y: val,
+                        color: color
+                    };
+                });
                 Highcharts.chart('medication-distribution', {
                     chart: {
                         type: 'bar',
@@ -412,8 +420,10 @@ ai.DashboardGC = {
                     legend: {enabled: false},
                     series: [{
                         name: '人數',
-                        data: medicationcounts,
-                        color: '#008687'
+                        data: medData,
+                        dataLabels: {
+                            enabled: true,
+                        }
                     }]
                 });
 
@@ -441,6 +451,7 @@ ai.DashboardGC = {
                         color: index < colors.length ? colors[index] : '#000000'
                     };
                 });
+
                 Highcharts.chart('arrhythmia-distribution', {
                     chart: {
                         type: 'pie',
@@ -489,6 +500,15 @@ ai.DashboardGC = {
                 let heart_rate = data.data.heart_rate_distribution.bins;
                 let heart_ratecounts = data.data.heart_rate_distribution.counts;
 
+                const maxValue = Math.max(...heart_ratecounts);
+                const heartData = heart_ratecounts.map(val => {
+                    const ratio = val / maxValue;
+                    const color = `hsl(180, 50%, ${55 - ratio * 30}%)`; 
+                    return {
+                        y: val,
+                        color: color
+                    };
+                });
                 Highcharts.chart('heart-rate-distribution', {
                     chart: {
                         type: 'column',
@@ -514,8 +534,10 @@ ai.DashboardGC = {
                     legend: {enabled: false},
                     series: [{
                         name: '人數',
-                        data: heart_ratecounts,
-                        color: '#008687'
+                        data: heartData,
+                        dataLabels: {
+                            enabled: true,
+                        }
                     }]
                 });
             })
